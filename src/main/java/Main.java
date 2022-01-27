1868b0fb36d13ad59469aab9f3e0e27727b6ef6d
Veronica <vera_danko93@hotmail.com>
2022-01-27 16:08:20 +0100

    Skapade terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        TerminalSize ts = new TerminalSize(58, 30);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        terminalFactory.setInitialTerminalSize(ts);
+        Terminal terminal = terminalFactory.createTerminal();
+        terminal.setCursorVisible(false);
     }
 }
